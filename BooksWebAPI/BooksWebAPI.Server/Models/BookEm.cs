292068e50680ec398e75766efb41bb95fe81a0ea
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 10:03:10 +0100

    added the type and the path for the books.xml as keys in the web.config

---
+++ BooksWebAPI/BooksWebAPI.Server/Models/BookEm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace BooksWebAPI.Server.Models
+{
+    /// <summary>
+    /// The Entity Model for a Book object. 
+    /// </summary>
+    public class BookEm
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Description { get; set; }
+    }
+}
