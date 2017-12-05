7fb9a1636da794d59776569ec98e0946f6e3234c
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 17:45:09 +0100

    added bookcontroller in client project

---
+++ BooksWebAPI/BooksWebAPI.Client/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksWebAPI.Client.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public Task<ActionResult> GetByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
