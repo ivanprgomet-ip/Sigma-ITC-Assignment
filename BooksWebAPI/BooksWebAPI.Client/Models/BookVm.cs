1bb2af513fd419509207693021bcd12955b15fd5
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 17:48:11 +0100

    book view model

---
+++ BooksWebAPI/BooksWebAPI.Client/Models/BookVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksWebAPI.Client.Models
+{
+    /// <summary>
+    /// The View Model for a Book object. 
+    /// </summary>
+    public class BookVm
+    {
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Description { get; set; }
+    }
+}
