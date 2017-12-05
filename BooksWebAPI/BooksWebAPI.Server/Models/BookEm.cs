9cb6f1041144365105c009fa14aeebd34faae62e
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 10:06:26 +0100

    summary

--- BooksWebAPI/BooksWebAPI.Server/Models/BookEm.cs
+++ BooksWebAPI/BooksWebAPI.Server/Models/BookEm.cs
@@ -8,16 +8,25 @@ namespace BooksWebAPI.Server.Models
 {
     /// <summary>
     /// The Entity Model for a Book object. 
+    /// The xml attributes for the properties are there to follow the convention of 
+    /// lower case letters for xml elements, just like in the books.xml file that was retrieved 
+    /// from Sigma ITC.
     /// </summary>
     public class BookEm
     {
         [XmlAttribute("id")]
         public string Id { get; set; }
+        [XmlAttribute("author")]
         public string Author { get; set; }
+        [XmlAttribute("title")]
         public string Title { get; set; }
+        [XmlAttribute("genre")]
         public string Genre { get; set; }
+        [XmlAttribute("price")]
         public decimal Price { get; set; }
+        [XmlAttribute("publish_date")]
         public DateTime PublishDate { get; set; }
+        [XmlAttribute("description")]
         public string Description { get; set; }
     }
 }
