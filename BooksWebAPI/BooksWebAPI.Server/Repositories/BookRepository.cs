68e40358321cb77950c35d71c3b54e3b61206fc7
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 10:58:09 +0100

    book repository for handing data calls to xml file and following repository pattern, and the ibookrepository for DI principle

---
+++ BooksWebAPI/BooksWebAPI.Server/Repositories/BookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooksWebAPI.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksWebAPI.Server.Repositories
+{
+    public class BookRepository:IBookRepository
+    {
+
+    }
+}
