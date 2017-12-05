0d2dc3764a21d67fdc4cce6b99be7da5c0c646a4
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-12-05 10:49:20 +0100

    catalog entity consisting of book ems created

---
+++ BooksWebAPI/BooksWebAPI.Server/Models/CatalogEm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace BooksWebAPI.Server.Models
+{
+    [XmlRoot("catalog")]
+    public class CatalogEm
+    {
+        public List<BookEm> Books { get; set; }
+    }
+}
